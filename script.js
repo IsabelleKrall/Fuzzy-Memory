ff302ec3446bee37dafdb015540f6c847e50263d
Isabelle Krall <isabellekrall@gmail.com>
2018-11-27 09:41:45 +0100

    replaced grid-container
    added the cards in array (script.js) instead

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+const cardsArray = [{
+    'name': 'anka',
+    'img': 'images/anka.svg',
+  },
+  {
+    'name': 'far',
+    'img': 'images/far.svg',
+  },
+  {
+    'name': 'get',
+    'img': 'images/get.svg',
+  },
+  {
+    'name': 'gris',
+    'img': 'images/gris.svg',
+  },
+  {
+    'name': 'hast',
+    'img': 'images/hast.svg',
+  },
+  {
+    'name': 'hund',
+    'img': 'images/hund.svg',
+  },
+  {
+    'name': 'kanin',
+    'img': 'images/kanin.svg',
+  },
+  {
+    'name': 'ko',
+    'img': 'images/ko.svg',
+  },
+  {
+    'name': 'skottkarra',
+    'img': 'images/skottkarra.svg',
+  },
+  {
+    'name': 'traktor',
+    'img': 'images/traktor.svg',
+  },
+
+];
+
+
+
+
+
 //Function for randomize the cards in card-box:
 var container = document.querySelector('.grid-container')
 
